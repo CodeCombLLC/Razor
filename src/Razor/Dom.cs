967eb2f37c65275a6e8be2cdbef5216bd3d6b518
あまみや ゆうこ <1@1234.sh>
2016-03-18 16:12:35 +0800

    Implementing

---
+++ src/Razor/Dom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor
+{
+    public enum CodeType
+    {
+        Html,
+        Razor,
+        Text
+    }
+
+    public class Dom
+    {
+        public CodeType Type { get; set; }
+
+        public string Begin { get; set; } 
+
+        public string End { get; set; }
+
+        public List<Dom> Children { get; set; } = new List<Dom>();
+
+        public void AppendChild(Dom dom)
+        {
+            Children.Add(dom);
+        }
+    }
+}
